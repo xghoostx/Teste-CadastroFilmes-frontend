b475be9a7d2bb4a74ed8d50759cdf44236fad70f
xghoostx <vasconcelosdeveloper@outlook.com>
2019-02-18 15:36:29 -0300

    Sistema do crud funcionando, entretando e necessario corrigir o erro ao atualizar a lista apos salvar

--- src/components/content/register/Register.js
+++ src/components/content/register/Register.js
@@ -1,85 +1,165 @@
-import React, { Component } from 'react';
-import { Col, Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import React, { Component } from 'react'
 import axios from '../../../services/api';
 
-const baseURL = 'http://localhost:3333';
 
-export default class Register extends Component {
-    state = {
-        nome: '',
-        descricao: '',
-        url: '',
-        lista: []
+const baseUrl = 'http://localhost:3333/filmes'
+const initialState = {
+    filme: { nome: '', descricao: '', url:'' },
+    lista: []
+}
+
+export default class UserCrud extends Component {
+
+    state = { ...initialState };
+
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ lista: resp.data });
+        });
     }
 
+    clear() {
+        this.setState({ filme: initialState.filme });
+    }
 
-    handleSaveFilme = async (e) => {
-        e.preventDefault();
-
-        const filme = this.state.lista;
-    
+    save() {
+        const filme = this.state.filme;
         const method = filme.id ? 'put' : 'post';
-        const url = filme.id ? `${this.baseUrl}/${filme.id}` : this.baseUrl;
-        const { data: post } = await axios[method]('/filmes', {
-            nome: this.state.nome,
-            descricao: this.state.descricao,
-            url: this.state.url
-        })
+        const url = filme.id ? `${baseUrl}/${filme.id}` : baseUrl;
+        axios[method](url, filme)
+            .then(resp => {
+                const lista = this.getUpdatedList(resp.data);
+                this.setState({ filme: initialState.filme, lista });
+            });
+    }
 
-        alert('Filme salvo com sucesso !');
+    getUpdatedList(filme, add = true) {
+        const lista = this.state.lista.filter(u => u.id !== filme.id);
+        if (add) filme.unshift(filme);
+        return lista;
+    }
 
+    updateField(event) {
+        const filme = { ...this.state.filme };
+        filme[event.target.name] = event.target.value;
+        this.setState({ filme });
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="nome"
+                                value={this.state.filme.nome}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Descrição</label>
+                            <textarea type="textarea" className="form-control"
+                                name="descricao"
+                                value={this.state.filme.descricao}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a descrição..." />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>url</label>
+                            <input type="text" className="form-control"
+                                name="url"
+                                value={this.state.filme.url}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a url..." />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-end">
+                            <button className="btn btn-primary"
+                                onClick={e => this.save(e)}>
+                                Salvar
+                        </button>
+
+                            <button className="btn btn-secondary ml-2"
+                                onClick={e => this.clear(e)}>
+                                Cancelar
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    load(filme) {
+        this.setState({ filme });
+    }
+
+    remove(filme) {
+        axios.delete(`${baseUrl}/${filme.id}`).then(resp => {
+            const lista = this.getUpdatedList(filme, false);
+            this.setState({ lista });
+        });
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Url</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        );
+    }
+
+    renderRows() {
+        return this.state.lista.map(filme => {
+            return (
+                <tr key={filme.id}>
+                    <td>{filme.nome}</td>
+                    <td>{filme.descricao}</td>
+                    <td><a href={`${filme.url}`} target="blank"> {filme.url}</a></td>
+                    <td>
+                        <button className="btn btn-warning"
+                            onClick={() => this.load(filme)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                            onClick={() => this.remove(filme)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
     }
 
     render() {
         return (
-            <aside className="register">
-                <Container id="form">
-                    <h1 id="title"> Cadastro de Filmes </h1>
-                    <Form onSubmit={this.handleSaveFilme}>
-                        <FormGroup row>
-                            <Label for="nome" sm={2}>Nome</Label>
-                            <Col sm={10}>
-                                <Input
-                                    onChange={e => this.setState({ nome: e.target.value })}
-                                    value={this.state.nome}
-                                    type="text" name="nome" id="nome" placeholder="Insira o nome do filme" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="descricao" sm={2}>Descrição</Label>
-                            <Col sm={10}>
-                                <Input
-                                    onChange={e => this.setState({ descricao: e.target.value })}
-                                  
-                                    type="textarea" name="descricao" id="descricao" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="url" sm={2}>Url</Label>
-                            <Col sm={10}>
-                                <Input
-                                    onChange={e => this.setState({ url: e.target.value })}
-                                    value={this.state.url}
-                                    type="text" name="url" id="url" placeholder="Insira a url do filme" />
-                            </Col>
-                        </FormGroup>
-                        {/* <FormGroup row>
-                            <Label for="capa" sm={2}>Capa</Label>
-                            <Col sm={10}>
-                                <Input type="file" name="capa" id="capa" />
-                                <FormText color="muted">
-                                    Por favor selecione uma imagem com extenção .png ou .jpg
-                        </FormText>
-                            </Col>
-                       </FormGroup> */}
-                        <FormGroup row>
-                            <Col sm={10}>
-                                <Button type="submit" id="btn-conf">Confirmar</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </Container>
-            </aside>
+            <div>
+                {this.renderForm()}
+                {this.renderTable()}
+            </div>
         );
     }
 }
